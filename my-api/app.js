90024df669661486e3b6fb41621e068ec71c8006
Henry Cooper <hcooper001@student.franciscan.edu>
2026-02-17 19:20:41 -0500

    added an array to test url endpoints

--- my-api/app.js
+++ my-api/app.js
@@ -4,10 +4,17 @@ const express = require('express');
 // create express app instance
 const app = express();
 
-// define route for http get requests to root url
-// this route sends a simple text response to client
-app.get('/', (req, res) => {
-    res.send('Welcome to my API'); // sends response
+// define array of product objects
+const products = [
+    { id: 1, name: "laptop", price: 1000 },
+    { id: 2, name: "phone", price: 500 }
+];
+
+
+// define route for http get requests to /api/v1/products
+// this route returns the list of products in JSON format
+app.get("/api/v1/products", (req, res) => {
+    res.json(products); // sends response
 });
 
 // start server on port 3000; log message to console
